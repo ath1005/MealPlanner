fec4d0d4c7ac5913872aed03c6b6892405d1f735
Atlee Hasson <hassonatlee@yahoo.com>
2020-05-11 16:35:15 -0400

    Initial commit

---
+++ MealPlanner/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+    'Create ADO.NET objects.
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private myReader As SqlDataReader
+    Private results As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        myConn = New SqlConnection("server = atleesql.database.windows.net; database = Nutrition; User ID=atlee; Password=B@seba1l73")
+        myCmd = myConn.CreateCommand
+        myCmd.CommandText = "SELECT Food FROM Food WHERE Calories = 95"
+        myConn.Open()
+        myReader = myCmd.ExecuteReader()
+        'Concatenate the query result into a string.
+        Do While myReader.Read()
+            results = results & myReader.GetString(0) & vbLf
+        Loop
+        '
+        MsgBox(results)
+        myReader.Close()
+        myConn.Close()
+    End Sub
+End Class
